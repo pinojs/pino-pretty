c15589bc1aa8643641c0978f417213186887597c
Ray Vincent <ray.vincent@blackswan.com>
2019-03-04 16:57:53 -0800

    Fix unreliable cli test working in cwd

--- test/cli-rc.test.js
+++ test/cli-rc.test.js
@@ -3,21 +3,32 @@
 const path = require('path')
 const spawn = require('child_process').spawn
 const test = require('tap').test
+const fs = require('fs')
+const rimraf = require('rimraf')
 
 const bin = require.resolve(path.join(__dirname, '..', 'bin.js'))
 const logLine = '{"level":30,"time":1522431328992,"msg":"hello world","pid":42,"hostname":"foo","v":1}\n'
-const rcFile = path.join(process.cwd(), '.pino-prettyrc.json')
 
 test('cli', (t) => {
-  t.afterEach((done) => {
-    // Update .pino-prettyrc.json with empty options
-    require('fs').writeFileSync(rcFile, JSON.stringify({}, null, 4))
+  let tmpDir
+  t.beforeEach((done) => {
+    tmpDir = path.join(__dirname, '.tmp' + Math.random())
+    fs.mkdirSync(tmpDir)
+    process.chdir(tmpDir)
     done()
+  })
+
+  t.afterEach((done) => {
+    rimraf(tmpDir, () => {
+      process.chdir(path.join(__dirname, '..'))
+      done()
+    })
   })
 
   t.test('loads and applies .pino-prettyrc.json', (t) => {
     t.plan(1)
     // Set translateTime: true on run configuration
+    let rcFile = path.join(process.cwd(), '.pino-prettyrc.json')
     require('fs').writeFileSync(rcFile, JSON.stringify({ translateTime: true }, null, 4))
     // Validate that the time has been translated
     const child = spawn(process.argv0, [bin])
