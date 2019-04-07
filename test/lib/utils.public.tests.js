42e5bc7a318cc587aeb39062d9dac12c9eac5526
James Sumners <james.sumners@gmail.com>
2019-04-07 09:01:28 -0400

    Add tests for prettifyMessage

--- test/lib/utils.public.tests.js
+++ test/lib/utils.public.tests.js
@@ -5,8 +5,10 @@ const getColorizer = require('../../lib/colors')
 const utils = require('../../lib/utils')
 
 tap.test('prettifyLevel', t => {
+  const { prettifyLevel } = utils
+
   t.test('returns `undefined` for unknown level', async t => {
-    const colorized = utils.prettifyLevel({log: {}})
+    const colorized = prettifyLevel({ log: {} })
     t.is(colorized, undefined)
   })
 
@@ -14,7 +16,7 @@ tap.test('prettifyLevel', t => {
     const log = {
       level: 30
     }
-    const colorized = utils.prettifyLevel({log})
+    const colorized = prettifyLevel({ log })
     t.is(colorized, 'INFO ')
   })
 
@@ -23,8 +25,46 @@ tap.test('prettifyLevel', t => {
       level: 30
     }
     const colorizer = getColorizer(true)
-    const colorized = utils.prettifyLevel({log, colorizer})
+    const colorized = prettifyLevel({ log, colorizer })
     t.is(colorized, '\u001B[32mINFO \u001B[39m')
+  })
+
+  t.end()
+})
+
+tap.test('prettifyMessage', t => {
+  const { prettifyMessage } = utils
+
+  t.test('returns `undefined` if `messageKey` not found', async t => {
+    const str = prettifyMessage({ log: {} })
+    t.is(str, undefined)
+  })
+
+  t.test('returns `undefined` if `messageKey` not string', async t => {
+    const str = prettifyMessage({ log: { msg: {} } })
+    t.is(str, undefined)
+  })
+
+  t.test('returns non-colorized value for default colorizer', async t => {
+    const str = prettifyMessage({ log: { msg: 'foo' } })
+    t.is(str, 'foo')
+  })
+
+  t.test('returns non-colorized value for alternate `messageKey`', async t => {
+    const str = prettifyMessage({ log: { message: 'foo' }, messageKey: 'message' })
+    t.is(str, 'foo')
+  })
+
+  t.test('returns colorized value for color colorizer', async t => {
+    const colorizer = getColorizer(true)
+    const str = prettifyMessage({ log: { msg: 'foo' }, colorizer })
+    t.is(str, '\u001B[36mfoo\u001B[39m')
+  })
+
+  t.test('returns colorized value for color colorizer for alternate `messageKey`', async t => {
+    const colorizer = getColorizer(true)
+    const str = prettifyMessage({ log: { message: 'foo' }, messageKey: 'message', colorizer })
+    t.is(str, '\u001B[36mfoo\u001B[39m')
   })
 
   t.end()
