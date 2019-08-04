e93a052a1fe84688f34d9ccf34d66a9a27af0100
Noam Kfir <noam@boundlesscode.com>
2019-08-04 22:27:49 +0300

    Extract built-in line builders to lib/line-builders modules

--- index.js
+++ index.js
@@ -4,15 +4,14 @@ const chalk = require('chalk')
 const colors = require('./lib/colors')
 const { ERROR_LIKE_KEYS, MESSAGE_KEY, TIMESTAMP_KEY } = require('./lib/constants')
 const {
-  prettifyErrorLog,
   prettifyLevel,
   prettifyMessage,
   prettifyMetadata,
-  prettifyObject,
   prettifyTime
 } = require('./lib/utils')
 
 const logBuilders = require('./lib/log-builders')
+const lineBuilders = require('./lib/line-builders')
 
 const defaultOptions = {
   colorize: chalk.supportsColor,
@@ -33,14 +32,13 @@ module.exports = function prettyFactory (options) {
   const IDENT = '    '
   const messageKey = opts.messageKey
   const timestampKey = opts.timestampKey
-  const errorLikeObjectKeys = opts.errorLikeObjectKeys
-  const errorProps = opts.errorProps.split(',')
 
   const colorizer = colors(opts.colorize)
 
   const context = {
     opts,
-    EOL
+    EOL,
+    IDENT
   }
 
   return pretty
@@ -68,80 +66,6 @@ module.exports = function prettyFactory (options) {
 
     context.log = log
     context.prettified = prettified
-
-    const lineBuilders = [
-      (lineParts, { prettified, opts }) => {
-        const { prettifiedLevel } = prettified
-        if (opts.levelFirst && prettifiedLevel) {
-          lineParts.push(prettifiedLevel)
-        }
-      },
-      (lineParts, { prettified }) => {
-        const { prettifiedTime } = prettified
-        if (prettifiedTime && lineParts.length === 0) {
-          lineParts.push(prettifiedTime)
-        } else if (prettifiedTime) {
-          lineParts.push(' ')
-          lineParts.push(prettifiedTime)
-        }
-      },
-      (lineParts, { prettified, opts }) => {
-        const { prettifiedLevel } = prettified
-        if (!opts.levelFirst && prettifiedLevel) {
-          if (lineParts.length > 0) {
-            lineParts.push(' ')
-          }
-          lineParts.push(prettifiedLevel)
-        }
-      },
-      (lineParts, { prettified }) => {
-        const { prettifiedMetadata } = prettified
-        if (prettifiedMetadata) {
-          lineParts.push(' ')
-          lineParts.push(prettifiedMetadata)
-          lineParts.push(':')
-        }
-      },
-      (lineParts) => {
-        if (lineParts.length > 0 && lineParts[lineParts.length - 1] !== ':') {
-          lineParts.push(':')
-        }
-      },
-      (lineParts, { prettified }) => {
-        const { prettifiedMessage } = prettified
-        if (prettifiedMessage) {
-          lineParts.push(' ')
-          lineParts.push(prettifiedMessage)
-        }
-      },
-      (lineParts, { EOL }) => {
-        if (lineParts.length > 0) {
-          lineParts.push(EOL)
-        }
-      },
-      (lineParts, { log, EOL }) => {
-        if (log.type === 'Error' && log.stack) {
-          const prettifiedErrorLog = prettifyErrorLog({
-            log,
-            errorLikeKeys: errorLikeObjectKeys,
-            errorProperties: errorProps,
-            ident: IDENT,
-            eol: EOL
-          })
-          lineParts.push(prettifiedErrorLog)
-        } else {
-          const skipKeys = typeof log[messageKey] === 'string' ? [messageKey] : undefined
-          const prettifiedObject = prettifyObject({
-            input: log,
-            skipKeys,
-            errorLikeKeys: errorLikeObjectKeys,
-            eol: EOL,
-            ident: IDENT
-          })
-          lineParts.push(prettifiedObject)
-        }
-      }
-    ]
 
     let lineParts = []
 
