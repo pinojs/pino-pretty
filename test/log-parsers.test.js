b1ca7d1017afc7dc08ea2aeb0cc75414baabef64
Noam Kfir <noam@boundlesscode.com>
2019-08-11 05:44:13 +0300

    Replace log parser output object with direct result and simplify the parseInput function

--- test/log-parsers.test.js
+++ test/log-parsers.test.js
@@ -14,26 +14,12 @@ test('extensible log parsers tests', (t) => {
       processors: [
         (input) => {
           executed = true
-          return { output: input }
+          return input
         }
       ]
     })
     const formatted = pretty(logLine)
     t.is(executed, true, `custom log parser was not executed: ${formatted}`)
-  })
-
-  t.test('use input for custom log parsers that return undefined result object', (t) => {
-    t.plan(1)
-    const builtInPretty = prettyFactory()
-    const customPretty = prettyFactory({
-      processors: [
-        ...defaultLogParsingSequence,
-        () => undefined
-      ]
-    })
-    const builtInOutput = builtInPretty(logLine)
-    const customOutput = customPretty(logLine)
-    t.is(customOutput, builtInOutput, `input did not pass through on undefined result`)
   })
 
   t.test('manually specifying built-in loggers is same as default behavior', (t) => {
