1c8c48da8b0a67c95e5fddedaa571efe32044e90
Dario Vladovic <d.vladimyr@gmail.com>
2019-11-01 15:59:29 +0100

    Refactor tests

--- test/cli-rc.test.js
+++ test/cli-rc.test.js
@@ -8,33 +8,24 @@ const rimraf = require('rimraf')
 
 const bin = require.resolve(path.join(__dirname, '..', 'bin.js'))
 const logLine = '{"level":30,"time":1522431328992,"msg":"hello world","pid":42,"hostname":"foo","v":1}\n'
+const noop = () => {}
 
 test('cli', (t) => {
-  let tmpDir
-  t.beforeEach((done) => {
-    tmpDir = path.join(__dirname, '.tmp' + Math.random())
-    fs.mkdirSync(tmpDir)
-    process.chdir(tmpDir)
-    done()
-  })
+  const tmpDir = path.join(__dirname, '.tmp_' + Date.now())
+  fs.mkdirSync(tmpDir)
 
-  t.afterEach((done) => {
-    rimraf(tmpDir, () => {
-      process.chdir(path.join(__dirname, '..'))
-      done()
-    })
-  })
+  t.tearDown(() => rimraf(tmpDir, noop))
 
   t.test('loads and applies .pino-prettyrc.json', (t) => {
     t.plan(1)
     // Set translateTime: true on run configuration
-    let rcFile = path.join(process.cwd(), '.pino-prettyrc.json')
-    require('fs').writeFileSync(rcFile, JSON.stringify({ translateTime: true }, null, 4))
+    const rcFile = path.join(tmpDir, '.pino-prettyrc.json')
+    fs.writeFileSync(rcFile, JSON.stringify({ translateTime: true }, null, 4))
     // Validate that the time has been translated
-    const child = spawn(process.argv0, [bin])
+    const child = spawn(process.argv0, [bin], { cwd: tmpDir })
     child.on('error', t.threw)
     child.stdout.on('data', (data) => {
-      t.is(data.toString(), `[2018-03-30 17:35:28.992 +0000] INFO  (42 on foo): hello world\n`)
+      t.is(data.toString(), '[2018-03-30 17:35:28.992 +0000] INFO  (42 on foo): hello world\n')
     })
     child.stdin.write(logLine)
     t.tearDown(() => child.kill())
@@ -43,13 +34,13 @@ test('cli', (t) => {
   t.test('loads and applies .pino-prettyrc.test.json', (t) => {
     t.plan(1)
     // Set translateTime: true on run configuration
-    let testRcFile = path.join(process.cwd(), '.pino-prettyrc.test.json')
-    require('fs').writeFileSync(testRcFile, JSON.stringify({ translateTime: true }, null, 4))
+    const testRcFile = path.join(tmpDir, '.pino-prettyrc.test.json')
+    fs.writeFileSync(testRcFile, JSON.stringify({ translateTime: true }, null, 4))
     // Validate that the time has been translated
-    const child = spawn(process.argv0, [bin, '-o', testRcFile])
+    const child = spawn(process.argv0, [bin, '-o', testRcFile], { cwd: tmpDir })
     child.on('error', t.threw)
     child.stdout.on('data', (data) => {
-      t.is(data.toString(), `[2018-03-30 17:35:28.992 +0000] INFO  (42 on foo): hello world\n`)
+      t.is(data.toString(), '[2018-03-30 17:35:28.992 +0000] INFO  (42 on foo): hello world\n')
     })
     child.stdin.write(logLine)
     t.tearDown(() => child.kill())
