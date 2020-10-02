820008c7735cd9a5918a05b361e2e4820f7b7142
Luis Faustino <luis.faustino@gmail.com>
2020-10-02 14:37:07 +0100

    Add levelLlabel option (#134)
    * Add levelLabel option; fix spacing issue on `INFO` and `WARN`;
    
    * Revert "Add levelLabel option; fix spacing issue on `INFO` and `WARN`;"
    
    This reverts commit 64e6301cc29d6ffab1e3590dcb8e09f9618b818b.
    
    * levelLabel option
    
    * fix short args option for levelLabel
    
    * README update
    
    * Remove ", use" from help/examples output

--- test/lib/utils.public.test.js
+++ test/lib/utils.public.test.js
@@ -101,6 +101,11 @@ tap.test('prettifyMessage', t => {
     t.is(str, 'appModule - ')
   })
 
+  t.test('returns message formatted by `messageFormat` option - levelLabel', async t => {
+    const str = prettifyMessage({ log: { msg: 'foo', context: 'appModule', level: 30 }, messageFormat: '[{level}] {levelLabel} {context} - {msg}' })
+    t.is(str, '[30] INFO  appModule - foo')
+  })
+
   t.test('`messageFormat` supports nested curly brackets', async t => {
     const str = prettifyMessage({ log: { level: 30 }, messageFormat: '{{level}}-{level}-{{level}-{level}}' })
     t.is(str, '{30}-30-{30-30}')
