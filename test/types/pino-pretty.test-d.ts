488b2cf352d2cc358b22a5abc8b4c76c9c69ba53
Igor Savin <iselwin@gmail.com>
2021-05-26 16:09:51 +0300

    Add TypeScript types (#186)

---
+++ test/types/pino-pretty.test-d.ts
@@ -0,0 +1,41 @@
+import prettyFactory from "../../";
+import {expectType} from "tsd";
+
+import PinoPretty, { PinoPretty as PinoPrettyNamed } from "../../";
+import PinoPrettyDefault from "../../";
+import * as PinoPrettyStar from "../../";
+import PinoPrettyCjsImport = require ("../../");
+import Prettifier = PinoPretty.Prettifier;
+const PinoPrettyCjs = require("../../");
+
+const options: PinoPretty.PrettyOptions = {
+  colorize: true,
+  crlf: false,
+  errorLikeObjectKeys: ["err", "error"],
+  errorProps: "",
+  hideObject: true,
+  levelKey: "level",
+  levelLabel: "foo",
+  messageFormat: false,
+  ignore: "",
+  levelFirst: false,
+  messageKey: "msg",
+  timestampKey: "timestamp",
+  translateTime: "UTC:h:MM:ss TT Z",
+  search: "foo == `bar`",
+  singleLine: false,
+};
+
+const pretty = prettyFactory(options);
+expectType<Prettifier>(pretty)
+
+expectType<Prettifier>(PinoPrettyNamed(options));
+expectType<Prettifier>(PinoPrettyDefault(options));
+expectType<Prettifier>(PinoPrettyStar.PinoPretty(options));
+expectType<Prettifier>(PinoPrettyStar.default(options));
+expectType<Prettifier>(PinoPrettyCjsImport.PinoPretty(options));
+expectType<Prettifier>(PinoPrettyCjsImport.default(options));
+expectType<any>(PinoPrettyCjs(options));
+
+expectType<string>(pretty({ foo: "bar" }));
+expectType<string>(pretty('dummy'));
