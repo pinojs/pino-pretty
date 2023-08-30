b200bea62f9269907231b382626fb435d64bd1e8
James Sumners <git@zifbang.com>
2023-08-30 14:49:08 -0400

    Refactor prettify-message to use context

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const abstractTransport = require('pino-abstract-transport')
 const colors = require('./lib/colors')
 const {
   ERROR_LIKE_KEYS,
+  LEVEL_KEY,
+  LEVEL_LABEL,
   MESSAGE_KEY,
   TIMESTAMP_KEY
 } = require('./lib/constants')
@@ -24,35 +26,16 @@ const pretty = require('./lib/pretty')
  * @property {boolean} [colorizeObjects=true] Apply coloring to rendered objects
  * when coloring is enabled.
  * @property {boolean} [crlf=false] End lines with `\r\n` instead of `\n`.
+ * @property {string|null} [customColors=null] A comma separated list of colors
+ * to use for specific level labels, e.g. `err:red,info:blue`.
+ * @property {string|null} [customLevels=null] A comma separated list of user
+ * defined level names and numbers, e.g. `err:99,info:1`.
+ * @property {CustomPrettifiers} [customPrettifiers={}] A set of prettifier
+ * functions to apply to keys defined in this object.
  * @property {K_ERROR_LIKE_KEYS} [errorLikeObjectKeys] A list of string property
  * names to consider as error objects.
  * @property {string} [errorProps=''] A comma separated list of properties on
  * error objects to include in the output.
- * @property {string|null} [customLevels=null] A comma separated list of user
- * defined level names and numbers, e.g. `err:99,info:1`.
- * @property {string|null} [customColors=null] A comma separated list of colors
- * to use for specific level labels, e.g. `err:red,info:blue`.
- * @property {boolean} [useOnlyCustomProps=true] When true, only custom levels
- * and colors will be used if they have been provided.
- * @property {boolean} [levelFirst=false] When true, the log level will be the
- * first field in the prettified output.
- * @property {string} [messageKey='msg'] Defines the key in incoming logs that
- * contains the message of the log, if present.
- * @property {null|MessageFormatString|MessageFormatFunction} [messageFormat=null]
- * When a string, defines how the prettified line should be formatted according
- * to defined tokens. When a function, a synchronous function that returns a
- * formatted string.
- * @property {undefined|string|number} [minimumLevel=undefined] The minimum
- * level for logs that should be processed. Any logs below this level will
- * be omitted.
- * @property {string} [timestampKey='time'] Defines the key in incoming logs
- * that contains the timestamp of the log, if present.
- * @property {boolean} [translateTime=true] When true, will translate a
- * JavaScript date integer into a human-readable string.
- * @property {object} [outputStream=process.stdout] The stream to write
- * prettified log lines to.
- * @property {CustomPrettifiers} [customPrettifiers={}] A set of prettifier
- * functions to apply to keys defined in this object.
  * @property {boolean} [hideObject=false] When `true`, data objects will be
  * omitted from the output (except for error objects).
  * @property {string} [ignore='hostname'] A comma separated list of log keys
@@ -60,9 +43,32 @@ const pretty = require('./lib/pretty')
  * @property {undefined|string} [include=undefined] A comma separated list of
  * log keys to include in the prettified log information. Only the keys in this
  * list will be included in the output.
+ * @property {boolean} [levelFirst=false] When true, the log level will be the
+ * first field in the prettified output.
+ * @property {string} [levelKey='level'] The key name in the log data that
+ * contains the level value for the log.
+ * @property {string} [levelLabel='levelLabel'] Token name to use in
+ * `messageFormat` to represent the name of the logged level.
+ * @property {null|MessageFormatString|MessageFormatFunction} [messageFormat=null]
+ * When a string, defines how the prettified line should be formatted according
+ * to defined tokens. When a function, a synchronous function that returns a
+ * formatted string.
+ * @property {string} [messageKey='msg'] Defines the key in incoming logs that
+ * contains the message of the log, if present.
+ * @property {undefined|string|number} [minimumLevel=undefined] The minimum
+ * level for logs that should be processed. Any logs below this level will
+ * be omitted.
+ * @property {object} [outputStream=process.stdout] The stream to write
+ * prettified log lines to.
  * @property {boolean} [singleLine=false] When `true` any objects, except error
  * objects, in the log data will be printed as a single line instead as multiple
  * lines.
+ * @property {string} [timestampKey='time'] Defines the key in incoming logs
+ * that contains the timestamp of the log, if present.
+ * @property {boolean} [translateTime=true] When true, will translate a
+ * JavaScript date integer into a human-readable string.
+ * @property {boolean} [useOnlyCustomProps=true] When true, only custom levels
+ * and colors will be used if they have been provided.
  */
 
 /**
@@ -74,23 +80,25 @@ const defaultOptions = {
   colorize: isColorSupported,
   colorizeObjects: true,
   crlf: false,
+  customColors: null,
+  customLevels: null,
+  customPrettifiers: {},
   errorLikeObjectKeys: ERROR_LIKE_KEYS,
   errorProps: '',
-  customLevels: null,
-  customColors: null,
-  useOnlyCustomProps: true,
-  levelFirst: false,
-  messageKey: MESSAGE_KEY,
-  messageFormat: null,
-  minimumLevel: undefined,
-  timestampKey: TIMESTAMP_KEY,
-  translateTime: true,
-  outputStream: process.stdout,
-  customPrettifiers: {},
   hideObject: false,
   ignore: 'hostname',
   include: undefined,
-  singleLine: false
+  levelFirst: false,
+  levelKey: LEVEL_KEY,
+  levelLabel: LEVEL_LABEL,
+  messageFormat: null,
+  messageKey: MESSAGE_KEY,
+  minimumLevel: undefined,
+  outputStream: process.stdout,
+  singleLine: false,
+  timestampKey: TIMESTAMP_KEY,
+  translateTime: true,
+  useOnlyCustomProps: true
 }
 
 /**
