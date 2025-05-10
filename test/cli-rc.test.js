b341dad74511d8334220a5209b3dbfff0d801e6f
Luca Del Puppo <luca@delpuppo.net>
2025-05-10 11:59:55 +0200

    test: move cli-rc.test.js to node test

--- test/cli-rc.test.js
+++ test/cli-rc.test.js
@@ -4,20 +4,21 @@ process.env.TZ = 'UTC'
 
 const path = require('node:path')
 const { spawn } = require('node:child_process')
-const { test } = require('tap')
+const { test } = require('node:test')
+const match = require('@jsumners/assert-match')
 const fs = require('node:fs')
 const { rimraf } = require('rimraf')
 
 const bin = require.resolve('../bin')
 const logLine = '{"level":30,"time":1522431328992,"msg":"hello world","pid":42,"hostname":"foo"}\n'
 
-test('cli', (t) => {
+test('cli', async (t) => {
   const tmpDir = path.join(__dirname, '.tmp_' + Date.now())
   fs.mkdirSync(tmpDir)
 
-  t.teardown(() => rimraf(tmpDir))
+  t.after(() => rimraf(tmpDir))
 
-  t.test('loads and applies default config file: pino-pretty.config.js', (t) => {
+  await t.test('loads and applies default config file: pino-pretty.config.js', async (t) => {
     t.plan(1)
     // Set translateTime: true on run configuration
     const configFile = path.join(tmpDir, 'pino-pretty.config.js')
@@ -25,18 +26,22 @@ test('cli', (t) => {
     const env = { TERM: 'dumb', TZ: 'UTC' }
     const child = spawn(process.argv[0], [bin], { env, cwd: tmpDir })
     // Validate that the time has been translated
-    child.on('error', t.threw)
-    child.stdout.on('data', (data) => {
-      t.equal(data.toString(), '[17:35:28.992] INFO (42): hello world\n')
+    child.on('error', t.assert.fail)
+    const endPromise = new Promise(resolve => {
+      child.stdout.on('data', (data) => {
+        t.assert.strictEqual(data.toString(), '[17:35:28.992] INFO (42): hello world\n')
+        resolve()
+      })
     })
     child.stdin.write(logLine)
-    t.teardown(() => {
+    await endPromise
+    t.after(() => {
       fs.unlinkSync(configFile)
       child.kill()
     })
   })
 
-  t.test('loads and applies default config file: pino-pretty.config.cjs', (t) => {
+  await t.test('loads and applies default config file: pino-pretty.config.cjs', async (t) => {
     t.plan(1)
     // Set translateTime: true on run configuration
     const configFile = path.join(tmpDir, 'pino-pretty.config.cjs')
@@ -47,19 +52,23 @@ test('cli', (t) => {
     const env = { TERM: ' dumb', TZ: 'UTC' }
     const child = spawn(process.argv[0], [bin], { env, cwd: tmpDir })
     // Validate that the time has been translated
-    child.on('error', t.threw)
-    child.stdout.on('data', (data) => {
-      t.equal(data.toString(), '[17:35:28.992] INFO (42): hello world\n')
+    child.on('error', t.assert.fail)
+    const endPromise = new Promise(resolve => {
+      child.stdout.on('data', (data) => {
+        t.assert.strictEqual(data.toString(), '[17:35:28.992] INFO (42): hello world\n')
+        resolve()
+      })
     })
     child.stdin.write(logLine)
-    t.teardown(() => {
+    await endPromise
+    t.after(() => {
       fs.unlinkSync(configFile)
       fs.unlinkSync(packageJsonFile)
       child.kill()
     })
   })
 
-  t.test('loads and applies default config file: .pino-prettyrc', (t) => {
+  await t.test('loads and applies default config file: .pino-prettyrc', async (t) => {
     t.plan(1)
     // Set translateTime: true on run configuration
     const configFile = path.join(tmpDir, '.pino-prettyrc')
@@ -67,18 +76,22 @@ test('cli', (t) => {
     const env = { TERM: ' dumb', TZ: 'UTC' }
     const child = spawn(process.argv[0], [bin], { env, cwd: tmpDir })
     // Validate that the time has been translated
-    child.on('error', t.threw)
-    child.stdout.on('data', (data) => {
-      t.equal(data.toString(), '[17:35:28.992] INFO (42): hello world\n')
+    child.on('error', t.assert.fail)
+    const endPromise = new Promise(resolve => {
+      child.stdout.on('data', (data) => {
+        t.assert.strictEqual(data.toString(), '[17:35:28.992] INFO (42): hello world\n')
+        resolve()
+      })
     })
     child.stdin.write(logLine)
-    t.teardown(() => {
+    await endPromise
+    t.after(() => {
       fs.unlinkSync(configFile)
       child.kill()
     })
   })
 
-  t.test('loads and applies default config file: .pino-prettyrc.json', (t) => {
+  await t.test('loads and applies default config file: .pino-prettyrc.json', async (t) => {
     t.plan(1)
     // Set translateTime: true on run configuration
     const configFile = path.join(tmpDir, '.pino-prettyrc.json')
@@ -86,18 +99,22 @@ test('cli', (t) => {
     const env = { TERM: ' dumb', TZ: 'UTC' }
     const child = spawn(process.argv[0], [bin], { env, cwd: tmpDir })
     // Validate that the time has been translated
-    child.on('error', t.threw)
-    child.stdout.on('data', (data) => {
-      t.equal(data.toString(), '[17:35:28.992] INFO (42): hello world\n')
+    child.on('error', t.assert.fail)
+    const endPromise = new Promise(resolve => {
+      child.stdout.on('data', (data) => {
+        t.assert.strictEqual(data.toString(), '[17:35:28.992] INFO (42): hello world\n')
+        resolve()
+      })
     })
     child.stdin.write(logLine)
-    t.teardown(() => {
+    await endPromise
+    t.after(() => {
       fs.unlinkSync(configFile)
       child.kill()
     })
   })
 
-  t.test('loads and applies custom config file: pino-pretty.config.test.json', (t) => {
+  await t.test('loads and applies custom config file: pino-pretty.config.test.json', async (t) => {
     t.plan(1)
     // Set translateTime: true on run configuration
     const configFile = path.join(tmpDir, 'pino-pretty.config.test.json')
@@ -105,15 +122,19 @@ test('cli', (t) => {
     const env = { TERM: ' dumb', TZ: 'UTC' }
     const child = spawn(process.argv[0], [bin, '--config', configFile], { env, cwd: tmpDir })
     // Validate that the time has been translated
-    child.on('error', t.threw)
-    child.stdout.on('data', (data) => {
-      t.equal(data.toString(), '[17:35:28.992] INFO (42): hello world\n')
+    child.on('error', t.assert.fail)
+    const endPromise = new Promise(resolve => {
+      child.stdout.on('data', (data) => {
+        t.assert.strictEqual(data.toString(), '[17:35:28.992] INFO (42): hello world\n')
+        resolve()
+      })
     })
     child.stdin.write(logLine)
-    t.teardown(() => child.kill())
+    await endPromise
+    t.after(() => child.kill())
   })
 
-  t.test('loads and applies custom config file: pino-pretty.config.test.js', (t) => {
+  await t.test('loads and applies custom config file: pino-pretty.config.test.js', async (t) => {
     t.plan(1)
     // Set translateTime: true on run configuration
     const configFile = path.join(tmpDir, 'pino-pretty.config.test.js')
@@ -121,16 +142,20 @@ test('cli', (t) => {
     const env = { TERM: ' dumb', TZ: 'UTC' }
     const child = spawn(process.argv[0], [bin, '--config', configFile], { env, cwd: tmpDir })
     // Validate that the time has been translated
-    child.on('error', t.threw)
-    child.stdout.on('data', (data) => {
-      t.equal(data.toString(), '[17:35:28.992] INFO (42): hello world\n')
+    child.on('error', t.assert.fail)
+    const endPromise = new Promise(resolve => {
+      child.stdout.on('data', (data) => {
+        t.assert.strictEqual(data.toString(), '[17:35:28.992] INFO (42): hello world\n')
+        resolve()
+      })
     })
     child.stdin.write(logLine)
-    t.teardown(() => child.kill())
+    await endPromise
+    t.after(() => child.kill())
   })
 
-  ;['--messageKey', '-m'].forEach((optionName) => {
-    t.test(`cli options override config options via ${optionName}`, (t) => {
+  await Promise.all(['--messageKey', '-m'].map(async (optionName) => {
+    await t.test(`cli options override config options via ${optionName}`, async (t) => {
       t.plan(1)
       // Set translateTime: true on run configuration
       const configFile = path.join(tmpDir, 'pino-pretty.config.js')
@@ -144,19 +169,23 @@ test('cli', (t) => {
       const env = { TERM: ' dumb', TZ: 'UTC' }
       const child = spawn(process.argv[0], [bin, optionName, 'new_msg'], { env, cwd: tmpDir })
       // Validate that the time has been translated and correct message key has been used
-      child.on('error', t.threw)
-      child.stdout.on('data', (data) => {
-        t.equal(data.toString(), '[17:35:28.992] INFO (42): hello world\n')
+      child.on('error', t.assert.fail)
+      const endPromise = new Promise(resolve => {
+        child.stdout.on('data', (data) => {
+          t.assert.strictEqual(data.toString(), '[17:35:28.992] INFO (42): hello world\n')
+          resolve()
+        })
       })
       child.stdin.write(logLine.replace(/"msg"/, '"new_msg"'))
-      t.teardown(() => {
+      await endPromise
+      t.after(() => {
         fs.unlinkSync(configFile)
         child.kill()
       })
     })
-  })
+  }))
 
-  t.test('cli options with defaults can be overridden by config', (t) => {
+  await t.test('cli options with defaults can be overridden by config', async (t) => {
     t.plan(1)
     // Set errorProps: '*' on run configuration
     const configFile = path.join(tmpDir, 'pino-pretty.config.js')
@@ -169,86 +198,101 @@ test('cli', (t) => {
     const env = { TERM: ' dumb', TZ: 'UTC' }
     const child = spawn(process.argv[0], [bin], { env, cwd: tmpDir })
     // Validate that the time has been translated and correct message key has been used
-    child.on('error', t.threw)
-    child.stdout.on('data', (data) => {
-      t.equal(data.toString(), '[21:31:36.006] FATAL: There was an error starting the process.\n    QueryError: Error during sql query: syntax error at or near SELECTT\n        at /home/me/projects/example/sql.js\n        at /home/me/projects/example/index.js\n    querySql: SELECTT * FROM "test" WHERE id = $1;\n    queryArgs: 12\n')
+    child.on('error', t.assert.fail)
+    const endPromise = new Promise(resolve => {
+      child.stdout.on('data', (data) => {
+        t.assert.strictEqual(data.toString(), '[21:31:36.006] FATAL: There was an error starting the process.\n    QueryError: Error during sql query: syntax error at or near SELECTT\n        at /home/me/projects/example/sql.js\n        at /home/me/projects/example/index.js\n    querySql: SELECTT * FROM "test" WHERE id = $1;\n    queryArgs: 12\n')
+        resolve()
+      })
     })
     child.stdin.write('{"level":60,"time":1594416696006,"msg":"There was an error starting the process.","type":"Error","stack":"QueryError: Error during sql query: syntax error at or near SELECTT\\n    at /home/me/projects/example/sql.js\\n    at /home/me/projects/example/index.js","querySql":"SELECTT * FROM \\"test\\" WHERE id = $1;","queryArgs":[12]}\n')
-    t.teardown(() => {
+    await endPromise
+    t.after(() => {
       fs.unlinkSync(configFile)
       child.kill()
     })
   })
 
-  t.test('throws on missing config file', (t) => {
+  await t.test('throws on missing config file', async (t) => {
     t.plan(2)
     const args = [bin, '--config', 'pino-pretty.config.missing.json']
     const env = { TERM: ' dumb', TZ: 'UTC' }
     const child = spawn(process.argv[0], args, { env, cwd: tmpDir })
-    child.on('close', (code) => t.equal(code, 1))
     child.stdout.pipe(process.stdout)
     child.stderr.setEncoding('utf8')
     let data = ''
     child.stderr.on('data', (chunk) => {
       data += chunk
     })
-    child.on('close', function () {
-      t.match(
-        data.toString(), 'Error: Failed to load runtime configuration file: pino-pretty.config.missing.json')
+    await new Promise(resolve => {
+      child.on('close', function (code) {
+        t.assert.strictEqual(code, 1)
+        match(
+          data.toString(), 'Error: Failed to load runtime configuration file: pino-pretty.config.missing.json', t)
+        resolve()
+      })
     })
-    t.teardown(() => child.kill())
+    t.after(() => child.kill())
   })
 
-  t.test('throws on invalid default config file', (t) => {
+  await t.test('throws on invalid default config file', async (t) => {
     t.plan(2)
     const configFile = path.join(tmpDir, 'pino-pretty.config.js')
     fs.writeFileSync(configFile, 'module.exports = () => {}')
     const env = { TERM: ' dumb', TZ: 'UTC' }
     const child = spawn(process.argv[0], [bin], { env, cwd: tmpDir })
-    child.on('close', (code) => t.equal(code, 1))
     child.stdout.pipe(process.stdout)
     child.stderr.setEncoding('utf8')
     let data = ''
     child.stderr.on('data', (chunk) => {
       data += chunk
     })
-    child.on('close', function () {
-      t.match(data, 'Error: Invalid runtime configuration file: pino-pretty.config.js')
+    await new Promise(resolve => {
+      child.on('close', function (code) {
+        t.assert.strictEqual(code, 1)
+        match(data, 'Error: Invalid runtime configuration file: pino-pretty.config.js', t)
+        resolve()
+      })
     })
-    t.teardown(() => child.kill())
+    t.after(() => child.kill())
   })
 
-  t.test('throws on invalid custom config file', (t) => {
+  await t.test('throws on invalid custom config file', async (t) => {
     t.plan(2)
     const configFile = path.join(tmpDir, 'pino-pretty.config.invalid.js')
     fs.writeFileSync(configFile, 'module.exports = () => {}')
     const args = [bin, '--config', path.relative(tmpDir, configFile)]
     const env = { TERM: ' dumb', TZ: 'UTC' }
     const child = spawn(process.argv[0], args, { env, cwd: tmpDir })
-    child.on('close', (code) => t.equal(code, 1))
     child.stdout.pipe(process.stdout)
     child.stderr.setEncoding('utf8')
     let data = ''
     child.stderr.on('data', (chunk) => {
       data += chunk
     })
-    child.on('close', function () {
-      t.match(data, 'Error: Invalid runtime configuration file: pino-pretty.config.invalid.js')
+    await new Promise(resolve => {
+      child.on('close', function (code) {
+        t.assert.strictEqual(code, 1)
+        match(data, 'Error: Invalid runtime configuration file: pino-pretty.config.invalid.js', t)
+        resolve()
+      })
     })
-    t.teardown(() => child.kill())
+    t.after(() => child.kill())
   })
 
-  t.test('test help', (t) => {
+  await t.test('test help', async (t) => {
     t.plan(1)
     const env = { TERM: ' dumb', TZ: 'UTC' }
     const child = spawn(process.argv[0], [bin, '--help'], { env })
     const file = fs.readFileSync('help/help.txt').toString()
-    child.on('error', t.threw)
-    child.stdout.on('data', (data) => {
-      t.equal(data.toString(), file)
-    })
-    t.teardown(() => child.kill())
-  })
+    child.on('error', t.assert.fail)
 
-  t.end()
+    await new Promise(resolve => {
+      child.stdout.on('data', (data) => {
+        t.assert.strictEqual(data.toString(), file)
+        resolve()
+      })
+    })
+    t.after(() => child.kill())
+  })
 })
