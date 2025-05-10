c90725592622cb0373b78ea99956be788934eb5a
Luca Del Puppo <luca@delpuppo.net>
2025-05-10 12:29:02 +0200

    test: move cli.test.js to node test

---
+++ test/helper.js
@@ -0,0 +1,10 @@
+function once (obj, event, fn) {
+  return new Promise(resolve => {
+    obj.on(event, (...args) => {
+      fn(...args)
+      resolve()
+    })
+  })
+}
+
+module.exports = { once }
