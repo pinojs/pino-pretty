1b3acd2646d3034a9d9fc0d4e6abcba3ea59f046
Luca Del Puppo <luca@delpuppo.net>
2025-05-14 07:54:22 +0200

    refactor: move to describe

--- test/cli.test.js
+++ test/cli.test.js
@@ -4,7 +4,7 @@ process.env.TZ = 'UTC'
 
 const path = require('node:path')
 const { spawn } = require('node:child_process')
-const { test } = require('node:test')
+const { describe, test } = require('node:test')
 const { once } = require('./helper')
 
 const bin = require.resolve(path.join(__dirname, '..', 'bin.js'))
@@ -13,8 +13,8 @@ const logLine = '{"level":30,"time":1522431328992,"msg":"hello world","pid":42,"
 const env = { TERM: 'dumb', TZ: 'UTC' }
 const formattedEpoch = '17:35:28.992'
 
-test('cli', async (t) => {
-  await t.test('does basic reformatting', async (t) => {
+describe('cli', () => {
+  test('does basic reformatting', async (t) => {
     t.plan(1)
     const child = spawn(process.argv[0], [bin], { env })
     child.on('error', t.assert.fail)
@@ -27,7 +27,7 @@ test('cli', async (t) => {
   })
 
   for (const optionName of ['--levelFirst', '-l']) {
-    await t.test(`flips epoch and level via ${optionName}`, async (t) => {
+    test(`flips epoch and level via ${optionName}`, async (t) => {
       t.plan(1)
       const child = spawn(process.argv[0], [bin, optionName], { env })
       child.on('error', t.assert.fail)
@@ -41,7 +41,7 @@ test('cli', async (t) => {
   }
 
   for (const optionName of ['--translateTime', '-t']) {
-    await t.test(`translates time to default format via ${optionName}`, async (t) => {
+    test(`translates time to default format via ${optionName}`, async (t) => {
       t.plan(1)
       const child = spawn(process.argv[0], [bin, optionName], { env })
       child.on('error', t.assert.fail)
@@ -55,7 +55,7 @@ test('cli', async (t) => {
   }
 
   for (const optionName of ['--ignore', '-i']) {
-    await t.test('does ignore multiple keys', async (t) => {
+    test('does ignore multiple keys', async (t) => {
       t.plan(1)
       const child = spawn(process.argv[0], [bin, optionName, 'pid,hostname'], { env })
       child.on('error', t.assert.fail)
@@ -69,7 +69,7 @@ test('cli', async (t) => {
   }
 
   for (const optionName of ['--customLevels', '-x']) {
-    await t.test(`customize levels via ${optionName}`, async (t) => {
+    test(`customize levels via ${optionName}`, async (t) => {
       t.plan(1)
       const logLine = '{"level":1,"time":1522431328992,"msg":"hello world","pid":42,"hostname":"foo"}\n'
       const child = spawn(process.argv[0], [bin, optionName, 'err:99,info:1'], { env })
@@ -82,7 +82,7 @@ test('cli', async (t) => {
       t.after(() => child.kill())
     })
 
-    await t.test(`customize levels via ${optionName} without index`, async (t) => {
+    test(`customize levels via ${optionName} without index`, async (t) => {
       t.plan(1)
       const logLine = '{"level":1,"time":1522431328992,"msg":"hello world","pid":42,"hostname":"foo"}\n'
       const child = spawn(process.argv[0], [bin, optionName, 'err:99,info'], { env })
@@ -95,7 +95,7 @@ test('cli', async (t) => {
       t.after(() => child.kill())
     })
 
-    await t.test(`customize levels via ${optionName} with minimumLevel`, async (t) => {
+    test(`customize levels via ${optionName} with minimumLevel`, async (t) => {
       t.plan(1)
       const child = spawn(process.argv[0], [bin, '--minimumLevel', 'err', optionName, 'err:99,info:1'], { env })
       child.on('error', t.assert.fail)
@@ -108,7 +108,7 @@ test('cli', async (t) => {
       t.after(() => child.kill())
     })
 
-    await t.test(`customize levels via ${optionName} with minimumLevel, customLevels and useOnlyCustomProps false`, async (t) => {
+    test(`customize levels via ${optionName} with minimumLevel, customLevels and useOnlyCustomProps false`, async (t) => {
       t.plan(1)
       const child = spawn(process.argv[0], [bin, '--minimumLevel', 'custom', '--useOnlyCustomProps', 'false', optionName, 'custom:99,info:1'], { env })
       child.on('error', t.assert.fail)
@@ -121,7 +121,7 @@ test('cli', async (t) => {
       t.after(() => child.kill())
     })
 
-    await t.test(`customize levels via ${optionName} with minimumLevel, customLevels and useOnlyCustomProps true`, async (t) => {
+    test(`customize levels via ${optionName} with minimumLevel, customLevels and useOnlyCustomProps true`, async (t) => {
       t.plan(1)
       const child = spawn(process.argv[0], [bin, '--minimumLevel', 'custom', '--useOnlyCustomProps', 'true', optionName, 'custom:99,info:1'], { env })
       child.on('error', t.assert.fail)
@@ -136,7 +136,7 @@ test('cli', async (t) => {
   }
 
   for (const optionName of ['--customColors', '-X']) {
-    await t.test(`customize levels via ${optionName}`, async (t) => {
+    test(`customize levels via ${optionName}`, async (t) => {
       t.plan(1)
       const child = spawn(process.argv[0], [bin, optionName, 'info:blue,message:red'], { env })
       child.on('error', t.assert.fail)
@@ -148,7 +148,7 @@ test('cli', async (t) => {
       t.after(() => child.kill())
     })
 
-    await t.test(`customize levels via ${optionName} with customLevels`, async (t) => {
+    test(`customize levels via ${optionName} with customLevels`, async (t) => {
       t.plan(1)
       const logLine = '{"level":1,"time":1522431328992,"msg":"hello world","pid":42,"hostname":"foo"}\n'
       const child = spawn(process.argv[0], [bin, '--customLevels', 'err:99,info', optionName, 'info:blue,message:red'], { env })
@@ -163,7 +163,7 @@ test('cli', async (t) => {
   }
 
   for (const optionName of ['--useOnlyCustomProps', '-U']) {
-    await t.test(`customize levels via ${optionName} false and customColors`, async (t) => {
+    test(`customize levels via ${optionName} false and customColors`, async (t) => {
       t.plan(1)
       const child = spawn(process.argv[0], [bin, '--customColors', 'err:blue,info:red', optionName, 'false'], { env })
       child.on('error', t.assert.fail)
@@ -175,7 +175,7 @@ test('cli', async (t) => {
       t.after(() => child.kill())
     })
 
-    await t.test(`customize levels via ${optionName} true and customColors`, async (t) => {
+    test(`customize levels via ${optionName} true and customColors`, async (t) => {
       t.plan(1)
       const child = spawn(process.argv[0], [bin, '--customColors', 'err:blue,info:red', optionName, 'true'], { env })
       child.on('error', t.assert.fail)
@@ -187,7 +187,7 @@ test('cli', async (t) => {
       t.after(() => child.kill())
     })
 
-    await t.test(`customize levels via ${optionName} true and customLevels`, async (t) => {
+    test(`customize levels via ${optionName} true and customLevels`, async (t) => {
       t.plan(1)
       const child = spawn(process.argv[0], [bin, '--customLevels', 'err:99,custom:30', optionName, 'true'], { env })
       child.on('error', t.assert.fail)
@@ -199,7 +199,7 @@ test('cli', async (t) => {
       t.after(() => child.kill())
     })
 
-    await t.test(`customize levels via ${optionName} true and no customLevels`, async (t) => {
+    test(`customize levels via ${optionName} true and no customLevels`, async (t) => {
       t.plan(1)
       const child = spawn(process.argv[0], [bin, optionName, 'true'], { env })
       child.on('error', t.assert.fail)
@@ -211,7 +211,7 @@ test('cli', async (t) => {
       t.after(() => child.kill())
     })
 
-    await t.test(`customize levels via ${optionName} false and customLevels`, async (t) => {
+    test(`customize levels via ${optionName} false and customLevels`, async (t) => {
       t.plan(1)
       const child = spawn(process.argv[0], [bin, '--customLevels', 'err:99,custom:25', optionName, 'false'], { env })
       child.on('error', t.assert.fail)
@@ -223,7 +223,7 @@ test('cli', async (t) => {
       t.after(() => child.kill())
     })
 
-    await t.test(`customize levels via ${optionName} false and no customLevels`, async (t) => {
+    test(`customize levels via ${optionName} false and no customLevels`, async (t) => {
       t.plan(1)
       const child = spawn(process.argv[0], [bin, optionName, 'false'], { env })
       child.on('error', t.assert.fail)
@@ -236,7 +236,7 @@ test('cli', async (t) => {
     })
   }
 
-  await t.test('does ignore escaped keys', async (t) => {
+  test('does ignore escaped keys', async (t) => {
     t.plan(1)
     const child = spawn(process.argv[0], [bin, '-i', 'log\\.domain\\.corp/foo'], { env })
     child.on('error', t.assert.fail)
@@ -249,7 +249,7 @@ test('cli', async (t) => {
     t.after(() => child.kill())
   })
 
-  await t.test('passes through stringified date as string', async (t) => {
+  test('passes through stringified date as string', async (t) => {
     t.plan(1)
     const child = spawn(process.argv[0], [bin], { env })
     child.on('error', t.assert.fail)
@@ -268,7 +268,7 @@ test('cli', async (t) => {
     t.after(() => child.kill())
   })
 
-  await t.test('end stdin does not end the destination', async (t) => {
+  test('end stdin does not end the destination', async (t) => {
     t.plan(2)
     const child = spawn(process.argv[0], [bin], { env })
     child.on('error', t.assert.fail)
@@ -288,7 +288,7 @@ test('cli', async (t) => {
   })
 
   for (const optionName of ['--timestampKey', '-a']) {
-    await t.test(`uses specified timestamp key via ${optionName}`, async (t) => {
+    test(`uses specified timestamp key via ${optionName}`, async (t) => {
       t.plan(1)
       const child = spawn(process.argv[0], [bin, optionName, '@timestamp'], { env })
       child.on('error', t.assert.fail)
@@ -303,7 +303,7 @@ test('cli', async (t) => {
   }
 
   for (const optionName of ['--singleLine', '-S']) {
-    await t.test(`singleLine=true via ${optionName}`, async (t) => {
+    test(`singleLine=true via ${optionName}`, async (t) => {
       t.plan(1)
       const logLineWithExtra = JSON.stringify(Object.assign(JSON.parse(logLine), {
         extra: {
@@ -323,7 +323,7 @@ test('cli', async (t) => {
     })
   }
 
-  await t.test('does ignore nested keys', async (t) => {
+  test('does ignore nested keys', async (t) => {
     t.plan(1)
 
     const logLineNested = JSON.stringify(Object.assign(JSON.parse(logLine), {
@@ -346,7 +346,7 @@ test('cli', async (t) => {
     t.after(() => child.kill())
   })
 
-  await t.test('change TZ', async (t) => {
+  test('change TZ', async (t) => {
     t.plan(1)
     const child = spawn(process.argv[0], [bin], { env: { ...env, TZ: 'Europe/Amsterdam' } })
     child.on('error', t.assert.fail)
