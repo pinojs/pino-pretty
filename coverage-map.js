156b63aa9c1cdaf5e78d6b874e1efac231e26b4a
mrazauskas <tom@mrazauskas.de>
2026-03-28 18:36:41 +0200

    chore: remove `coverage-map.js`

--- coverage-map.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-module.exports = testFile => {
-  // Ignore coverage on files that do not have a direct corollary.
-  if (testFile.startsWith('test/')) return false
-
-  // Indicate the matching name, sans '.test.js', should be checked for coverage.
-  return testFile.replace(/\.test\.js$/, '.js')
-}
